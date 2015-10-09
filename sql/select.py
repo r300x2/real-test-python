b55ce84e37a54f7786e179161be66a1a9ccc0239
Jon Wayne Yu <r300x2@gmail.com>
2015-10-09 06:32:55 -0400

    added more sql code

---
+++ sql/select.py
@@ -0,0 +1,13 @@
+# SELECT statement, remove unicode characters
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+
+	c.execute("SELECT firstname, lastname FROM employees")
+
+	rows = c.fetchall()
+
+	for r in rows:
+		print r[0], r[1]

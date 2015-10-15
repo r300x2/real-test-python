8e86c8bdadd5d06f2e938091e71372399b2ec1f8
Jon Wayne Yu <r300x2@gmail.com>
2015-10-14 21:18:34 -0400

    flask-blog

---
+++ flask-blog/blog.py
@@ -0,0 +1,54 @@
+# blog.py - controller
+
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
+from functools import wraps
+
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\xf5\xa8\xedR\xfb\xa5^#xx02@\xb8\xa4\x1fdx812x88\xd22lg4x04\xc81\xad\xc3'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again.'
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error)
+
+@app.route('/main')
+@login_required
+def main():
+	return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
+	
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
